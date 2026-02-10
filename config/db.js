84e9b8e62f78f49b11cf937613c58d54e146b672
WNR28 <wahyunoerrahmat980@gmail.com>
2026-02-10 10:17:24 +0700

    Use DATABASE_URL for Supabase connection

--- config/db.js
+++ config/db.js
@@ -1,11 +1,7 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false,
   },
